e949e4ec6781b63e6e9aba7c12cf3e784d7c5b96
Scott(Xiaoyang) Chen <xiaoyan5@andrew.cmu.edu>
2026-05-28 05:23:38 -0700

    Add extension icons to manifest

--- manifest.config.ts
+++ manifest.config.ts
@@ -6,7 +6,19 @@ export default defineManifest({
   name: packageJson.displayName,
   version: packageJson.version,
   description: packageJson.description,
+  icons: {
+    16: 'src/assets/icons/icon16.png',
+    32: 'src/assets/icons/icon32.png',
+    48: 'src/assets/icons/icon48.png',
+    128: 'src/assets/icons/icon128.png',
+  },
   action: {
+    default_icon: {
+      16: 'src/assets/icons/icon16.png',
+      32: 'src/assets/icons/icon32.png',
+      48: 'src/assets/icons/icon48.png',
+      128: 'src/assets/icons/icon128.png',
+    },
     default_popup: 'index.html',
     default_title: packageJson.displayName,
   },
