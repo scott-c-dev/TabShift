83d089c1400c8e7d7d95f8094d3e8a7a25b891ca
Scott(Xiaoyang) Chen <xiaoyan5@andrew.cmu.edu>
2026-05-28 05:22:49 -0700

    Centralize extension metadata

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,13 +1,14 @@
 import { defineManifest } from '@crxjs/vite-plugin'
+import packageJson from './package.json'
 
 export default defineManifest({
   manifest_version: 3,
-  name: 'Divactive Tab Manager',
-  version: '0.0.0',
-  description: 'Internal Chrome extension for managing tabs across windows.',
+  name: packageJson.displayName,
+  version: packageJson.version,
+  description: packageJson.description,
   action: {
     default_popup: 'index.html',
-    default_title: 'Divactive Tab Manager',
+    default_title: packageJson.displayName,
   },
   background: {
     service_worker: 'src/background.ts',
