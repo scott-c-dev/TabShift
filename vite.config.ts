529d2c1849c2bcf17c6053b5e53e076925ce5e29
Scott(Xiaoyang) Chen <xiaoyan5@andrew.cmu.edu>
2026-05-28 02:00:30 -0700

    Add Tailwind and remove Vite template UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import babel from '@rolldown/plugin-babel'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.config'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    cors: true,
+  },
   plugins: [
     react(),
+    tailwindcss(),
     babel({
       presets: [reactCompilerPreset()],
     }),
